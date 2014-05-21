2ccea2631664e3719f53a99dd1c0117e7092df5a
binbin <binbin@mogujie.com>
2014-05-21 18:03:04 +0800

    binbin:1

--- packages/dashboard/server/models/organization.js
+++ packages/dashboard/server/models/organization.js
@@ -23,6 +23,9 @@ var OrganizationSchema = new Schema({
         type: String,
         required: true
     },
+    members:{
+        type:Array
+    },
     created: {
         date: Date
     },
@@ -33,3 +36,27 @@ var OrganizationSchema = new Schema({
 });
 
 mongoose.model('Organization', OrganizationSchema);
+
+var Organization = mongoose.model('Organization');
+
+/**
+ * 同一个用户只能建立统一个组织
+ */
+OrganizationSchema.pre('save', function(next) {
+    var name = this.name;
+    var owner = this.owner;
+
+    //校验下是否已经有了
+    Organization.find({name:name,owner:owner},function(err,result){
+        if(err){
+            next(err);
+        }else{
+            if(result.length > 0){
+                next(new Error('has organization['+name+'] of owner['+owner+']'));
+            }else{
+                next();
+            }
+        }
+    });
+});
+
