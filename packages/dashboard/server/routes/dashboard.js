666dbfadaa188bfa6146df85f33a19cfdafb8528
binbin <binbin@mogujie.com>
2014-05-23 17:57:52 +0800

    binbin:post 2 get

--- packages/dashboard/server/routes/dashboard.js
+++ packages/dashboard/server/routes/dashboard.js
@@ -44,15 +44,22 @@ module.exports = function(Dashboard, app, auth, database) {
     app.post('/api/organization/create', auth.requiresLogin, organization.create);
     app.post('/api/organization/del', auth.requiresLogin, organization.del);
     app.post('/api/organization/modify', auth.requiresLogin, organization.modify);
-    app.get('/api/organizations/:page', auth.requiresLogin, organization.fetchByMember);
-    app.get('/api/organizations/me/:page', auth.requiresLogin, organization.fetchByOwner);
+    app.get('/api/organizations', auth.requiresLogin, organization.fetchByMember);
+    app.get('/api/organizations/me', auth.requiresLogin, organization.fetchByOwner);
 
     // 项目相关
     var project = require('../controllers/project.js');
     app.post('/project/create', auth.requiresLogin, project.create);
     app.post('/project/del', auth.requiresLogin, project.del);
-    app.post('/project/fetch', auth.requiresLogin, project.fetch);
-    app.post('/project/fetchOne', auth.requiresLogin, project.fetchOne);
+    app.get('/project/fetch', auth.requiresLogin, project.fetch);
+    app.get('/project/fetchOne', auth.requiresLogin, project.fetchOne);
+
+
+    //module相关
+    var modules = require('../controllers/modules.js');
+    app.post('/modules/save', auth.requiresLogin, modules.save);
+    app.get('/modules/fetch',auth.requiresLogin,modules.fetch);
+    app.get('/modules/fetchofversion',auth.requiresLogin,modules.fetchOfVersion);
 
 };
 
