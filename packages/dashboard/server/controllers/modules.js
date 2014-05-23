a71fa34b2bda06b08fea0ba36d9eea9dbe5c94aa
binbin <binbin@mogujie.com>
2014-05-23 17:59:57 +0800

    binbin:x

---
+++ packages/dashboard/server/controllers/modules.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Modules = mongoose.model('Modules');
+
+exports.save = function(req,res){
+
+    var modules = new Modules(req.body);
+    var owner = req.user.username;
+    modules.owner = owner;
+    modules.save(function(err){
+
+        if (err) {
+            switch (err.code) {
+                default:
+                    res.status(400).send('Please fill all the required fields');
+            }
+            return res.status(400);
+        }else{
+            res.status(200);
+            return res.send("success");
+        }
+    })
+
+}
+
+exports.fetch = function(req,res){
+    var projectName = req.body.projectName;
+    var organizationName = req.body.organizationName;
+    if(!organizationName){
+        organizationName = null;
+    }
+    var owner = req.user.username;
+
+    Modules.find({projectName:projectName,organizationName:organizationName,owner:owner},function(err,modules){
+        if (err) {
+            switch (err.code) {
+                default:
+                    res.status(400).send('Please fill all the required fields');
+            }
+            return res.status(400);
+        }else{
+            res.status(200);
+            return res.send(modules);
+        }
+    });
+}
+
+exports.fetchOfVersion = function(req,res){
+    var projectName = req.body.projectName;
+    var organizationName = req.body.organizationName;
+    if(!organizationName){
+        organizationName = null;
+    }
+    var owner = req.user.username;
+    var version = req.body.version;
+
+    Modules.findOne({projectName:projectName,organizationName:organizationName,owner:owner,version:version},function(err,module){
+        if (err) {
+            switch (err.code) {
+                default:
+                    res.status(400).send('Please fill all the required fields');
+            }
+            return res.status(400);
+        }else{
+            res.status(200);
+            return res.send(module);
+        }
+    });
+}
