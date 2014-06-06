2b663e62c83dd02d075d324d9dc74bb8d91aa671
binbin <binbin@mogujie.com>
2014-06-06 14:50:02 +0800

    binbin

--- test/mocha/dashboard/project/controller.js
+++ test/mocha/dashboard/project/controller.js
@@ -12,8 +12,9 @@ var cookie;
 var postData;
 var postNoOrganizationData;
 var project;
+var first_version;
 
-describe('<dashboard organization controller test', function () {
+describe('关于项目的测试', function () {
     before(function (done) {
         postData = {
             name: "滨滨项目-1",
@@ -32,8 +33,8 @@ describe('<dashboard organization controller test', function () {
 
     });
 
-    describe('project test', function () {
-        it('should create project', function (done) {
+    describe('project 场景', function () {
+        it('能够创建一个项目', function (done) {
             var request = superagent.agent();
             request.post('http://localhost:3000/project/create')
                 .send(postData)
@@ -45,7 +46,7 @@ describe('<dashboard organization controller test', function () {
                 });
         });
 
-        it('should add modules',function(done){
+        it('能够为项目添加模块-全量',function(done){
             var modules = {
                 name:'lal',
                 desc:'dd',
@@ -62,7 +63,7 @@ describe('<dashboard organization controller test', function () {
                         desc:'ssasa',
                         type: 'd',
                         request_type:'c',
-                        request_url:"lkul",
+                        request_url:"/style/blog",
                         map_url:'bulul',
                         request_parameter:{
 
@@ -92,9 +93,80 @@ describe('<dashboard organization controller test', function () {
         });
 
 
-        it('query project of project id', function (done) {
+        it('根据项目编号查询项目详情', function (done) {
             var request = superagent.agent();
             request.get('http://localhost:3000/project/fetchOne')
+                .send({projectId:project._id})
+                .set('cookie', cookie)
+                .end(function (res) {
+                    res.status.should.equal(200);
+                    first_version = res.body.version;
+                    done();
+                });
+        });
+
+        it('修改项目模块-全量',function(done){
+            var modules = {
+                name:'lal',
+                desc:'dd',
+                projectId:'333',
+                version:'222',
+                author:"ben",
+                type:"aa",
+                items:[{
+                    name:'',
+                    template:"aasjdf",
+                    type:"jkd",
+                    items:[{
+                        name:'sdf',
+                        desc:'ssasa',
+                        type: 'd',
+                        request_type:'c',
+                        request_url:"/style/blog",
+                        map_url:'bulul',
+                        request_parameter:{
+
+                        },
+                        response_parameter:{
+                            userId:{desc:'用户id',value:'number',remark:'haha'},
+                            userName:{desc:'用户名',
+                                value:[{
+                                    key1:{value:'a',remark:'@time'},key2:{value:'b'}
+                                }],
+                                remark:'lal'},
+                            isDeleted:{value:'boolean'}
+                        }
+                    }]
+                }]
+            };
+
+            var request = superagent.agent();
+            request.get('http://localhost:3000/project/addmodules')
+                .send({projectId:project._id,modules:modules})
+                .set('cookie', cookie)
+                .end(function (res) {
+                    res.status.should.equal(200);
+                    done();
+                });
+
+        });
+
+        it('更新后的项目版本号加1', function (done) {
+            var request = superagent.agent();
+            request.get('http://localhost:3000/project/fetchOne')
+                .send({projectId:project._id})
+                .set('cookie', cookie)
+                .end(function (res) {
+                    res.status.should.equal(200);
+                    var current_version = res.body.version;
+                    current_version.should.equal(first_version+1);
+                    done();
+                });
+        });
+
+        it('查看当前项目的版本历史',function(done){
+            var request = superagent.agent();
+            request.get('http://localhost:3000/project/queryVersion')
                 .send({projectId:project._id})
                 .set('cookie', cookie)
                 .end(function (res) {
@@ -103,7 +175,29 @@ describe('<dashboard organization controller test', function () {
                 });
         });
 
-        it('query all project', function (done) {
+        it('查看某个版本的项目',function(done){
+            var request = superagent.agent();
+            request.get('http://localhost:3000/project/viewVersion')
+                .send({projectId:project._id,version:first_version})
+                .set('cookie', cookie)
+                .end(function (res) {
+                    res.status.should.equal(200);
+                    done();
+                });
+        });
+
+        it('回滚到某个版本号',function(done){
+            var request = superagent.agent();
+            request.get('http://localhost:3000/project/changeVersion')
+                .send({projectId:project._id,version:first_version})
+                .set('cookie', cookie)
+                .end(function (res) {
+                    res.status.should.equal(200);
+                    done();
+                });
+        });
+
+        it('查看当前用户的所有项目', function (done) {
             var request = superagent.agent();
             request.get('http://localhost:3000/project/fetch')
                 .send({})
@@ -115,23 +209,34 @@ describe('<dashboard organization controller test', function () {
                     done();
                 });
         });
+
+        it('项目归档',function (done){
+            var request = superagent.agent();
+            request.get('http://localhost:3000/project/save2File')
+                .send({projectId:project._id})
+                .set('cookie',cookie)
+                .end(function (res) {
+                    res.status.should.equal(200);
+                    done();
+                });
+        });
     });
 
-    describe('mock data test', function () {
-        it('should get problem use miss request url', function (done) {
+    describe('模拟数据', function () {
+        it('错误的url获取模拟数据会出错', function (done) {
             var request = superagent.agent();
             request.get('http://localhost:3000/mockjs/generatedata')
-                .send({projectId:project._id,requestUrl:'lkul1111'})
+                .send({projectId:project._id,requestUrl:'/style/star'})
                 .end(function (res) {
                     res.status.should.equal(400);
                     done();
                 });
         });
 
-        it('should get mock data', function (done) {
+        it('正确的url获取模拟数据是正确的', function (done) {
             var request = superagent.agent();
             request.get('http://localhost:3000/mockjs/generatedata')
-                .send({projectId:project._id,requestUrl:'lkul'})
+                .send({projectId:project._id,requestUrl:'/style/blog'})
                 .end(function (res) {
                     res.status.should.equal(200);
                     console.log("********************************");
@@ -145,14 +250,15 @@ describe('<dashboard organization controller test', function () {
     });
 
     after(function (done) {
-        var request = superagent.agent();
-        request.post('http://localhost:3000/project/del')
-            .send({projectId:project._id})
-            .set('cookie', cookie)
-            .end(function (res) {
-                res.status.should.equal(200);
-                done();
-            });
+//        var request = superagent.agent();
+//        request.post('http://localhost:3000/project/del')
+//            .send({projectId:project._id})
+//            .set('cookie', cookie)
+//            .end(function (res) {
+//                res.status.should.equal(200);
+//                done();
+//            });
+        done();
     });
 });
 
